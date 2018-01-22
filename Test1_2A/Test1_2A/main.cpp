73e072f76daf6969de5e08e5e1f0086c685d4fe4
huangkeke <huangkekehkk@163.com>
2018-01-22 22:27:43 +0800

    第一个窗口程序

---
+++ Test1_2A/Test1_2A/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc,argv);
+	QLabel label("Hello World");
+	label.show();
+	return a.exec();
+}
+
+//Qt5中QtGui是 QtWidgets 中文
