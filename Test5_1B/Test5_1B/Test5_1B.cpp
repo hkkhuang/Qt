d749563329cfece37c5582550ed78baa84b3a46e
huangkeke <hkkhuang@163.com>
2018-01-27 20:40:48 +0800

    复选框_QCheckedBox_时间处理

---
+++ Test5_1B/Test5_1B/Test5_1B.cpp
@@ -0,0 +1,24 @@
+﻿#include "Test5_1B.h"
+
+Test5_1B::Test5_1B(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+	//信号槽连接
+	connect(ui.cbxAgree, SIGNAL(stateChanged(int)),
+		this, SLOT(OnCheckChanged()));
+}
+
+int Test5_1B::OnCheckChanged()
+{
+	if (ui.cbxAgree->isChecked())
+	{
+		ui.btnNext->setEnabled(true);
+	} 
+	else
+	{
+		ui.btnNext->setEnabled(false);
+	}
+	
+	return 0;
+}
---
+++ Test5_1B/Test5_1B/Test5_1B.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_Test5_1B.h"
+
+class Test5_1B : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	Test5_1B(QWidget *parent = Q_NULLPTR);
+//添加槽
+private slots:
+int OnCheckChanged();
+
+private:
+	Ui::Test5_1BClass ui;
+};
