8d81490102f0bab3ce2b7436bb0e5d756ed48220
huangkeke <hkkhuang@163.com>
2018-02-05 21:57:21 +0800

    界面线程

---
+++ Test8_1A/Test8_1A/main.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+
+// 包含windows的头文件，用于测试Sleep
+#include <winsock2.h>
+#include <windows.h>
+
+
+int HandleCommand()
+{
+	printf("begin .............");
+	::Sleep(10 * 1000); // 单位：毫秒
+	printf(".............end \n");
+	return 0;
+}
+
+int main()
+{
+	// 主控循环
+	while (1)
+	{
+		printf("> ");
+		char cmdline[128]; // 命名行
+		gets_s(cmdline);
+
+
+		// 处理命令
+		printf("处理中 .... \n");
+		if (strcmp(cmdline, "1") == 0)
+		{
+			HandleCommand();
+		}
+		printf("处理完成\n\n");
+	}
+}
+
+
